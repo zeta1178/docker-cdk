b3845d9599853f429706dd89f0315a94bd46547a
zeta1178 <michaelcruz@aim.com>
2023-08-02 10:58:24 -0400

    testing export vars

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,3 @@
-
-
 # Dockerfile to run aws cdk commands
 # References
 # - AWS CLI https://levelup.gitconnected.com/how-to-create-a-simple-docker-image-with-aws-cli-and-serverless-installed-d1cc2901946
@@ -9,29 +7,6 @@ ARG AWS_ACCESS_KEY_ID
 ARG AWS_SECRET_ACCESS_KEY 
 ARG AWS_SESSION_TOKEN
 
-# Install packages
-RUN apk update && apk add --update --no-cache \
-    git \
-    bash \
-    curl \
-    openssh \
-    python3 \
-    py3-pip \
-    py-cryptography \
-    wget \
-    curl \
-    nodejs \
-    npm
-RUN apk --no-cache add --virtual builds-deps build-base python3
-# Update NPM
-RUN npm config set unsafe-perm true
-RUN npm update -g
-# Upgrade pip install cli
-RUN pip install --upgrade pip && \
-    pip install --upgrade awscli
-# Install cdk
-RUN npm install -g aws-cdk 
-
 #WORKDIR root/
 #RUN mkdir -p .aws/sso/cache
 #COPY ./config/config .aws/config
@@ -39,7 +14,9 @@ RUN npm install -g aws-cdk
 
 WORKDIR /home/app
 COPY ./app /home/app
-RUN pip install -r requirements.txt
-RUN cdk deploy --all --require-approval never
+
+RUN echo $AWS_ACCESS_KEY_ID
+RUN echo $AWS_SECRET_ACCESS_KEY
+RUN echo $AWS_SESSION_TOKEN
 
 ENTRYPOINT []
--- cred.sh
+++ cred.sh
@@ -2,11 +2,20 @@
 unset AWS_ACCESS_KEY_ID
 unset AWS_SECRET_ACCESS_KEY
 unset AWS_SESSION_TOKEN
-#ASSUME_ROLE_ARN="arn:aws:iam::992690408789:role/aws-reserved/sso.amazonaws.com/AWSReservedSSO_AdministratorAccess_8773fea6ec8d21df"
-#TEMP_ROLE=$(aws sts assume-role --role-arn $ASSUME_ROLE_ARN --role-session-name role)
-rm -f $PWD/role.txt 
-TEMP_ROLE=$(aws sts get-caller-identity | tee $PWD/role.txt)
-export TEMP_ROLE
+ASSUME_ROLE_ARN="arn:aws:iam::992690408789:role/aws-reserved/sso.amazonaws.com/AWSReservedSSO_AdministratorAccess_8773fea6ec8d21df"
+TEMP_ROLE=$(aws sts assume-role --role-arn $ASSUME_ROLE_ARN --role-session-name role)
+rm -f $PWD/role.txt
+#aws sts assume-role --role-arn "arn:aws:iam::992690408789:role/CrossAccountAccessCodeCommitAccessRoleForAWSProServ" --role-session-name "amtrak" | tee $PWD/role.txt
+#TEMP_ROLE=$(aws sts get-caller-identity | tee $PWD/role.txt)
+#export TEMP_ROLE
+
+# ASSUME_ROLE_ARN="arn:aws:iam::$account_id:role/Secretassumerole"
+# TEMP_ROLE=$(aws sts assume-role --role-arn $ASSUME_ROLE_ARN --role-session-name test)
+# export TEMP_ROLE
+# export AWS_ACCESS_KEY_ID=$(echo "${TEMP_ROLE}" | jq -r '.Credentials.AccessKeyId')
+# export AWS_SECRET_ACCESS_KEY=$(echo "${TEMP_ROLE}" | jq -r '.Credentials.SecretAccessKey')
+# export AWS_SESSION_TOKEN=$(echo "${TEMP_ROLE}" | jq -r '.Credentials.SessionToken')
+
 
 #source $PWD/test.env
 # export mike3=hotdog
@@ -20,6 +29,6 @@ export TEMP_ROLE
 #export AWS_SESSION_TOKEN=$(cat $PWD/role.txt  | jq -r '.UserId')
 #echo $(cat $PWD/role.txt  | jq -r '.UserId')
 #export mike3=$(cat $PWD/role.txt  | jq -r '.UserId')
-export mike3="hotdog"
+#export mike3="hotdog"
 
 #aws sts assume-role --role-arn arn:aws:iam::992690408789:role/aws-reserved/sso.amazonaws.com/AWSReservedSSO_AdministratorAccess_8773fea6ec8d21df --role-session-name role
